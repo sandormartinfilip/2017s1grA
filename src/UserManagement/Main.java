3284eeeddf14338dcd6db53e00f66f4b0fca605d
Damira Petrean <petrean.damira@gmail.com>
2017-07-12 11:50:03 +0300

    jdbc updated

--- src/UserManagement/Main.java
+++ src/UserManagement/Main.java
@@ -27,18 +27,27 @@ public class Main {
 						conn = DriverManager.getConnection(DB_URL, USER, PASS );
 						System.out.println("Connected database successfully...");
 						
-						userDAO.addUser(new User(1, "Damira", "Petrean", "0741101456", "mail@mail.com", "damirapetrean", "dami", 1) , conn);
+						//insert into database
+						//userDAO.addUser(new User(1, "Damira", "Petrean", "0741101456", "mail@mail.com", "damirapetrean", "dami", 1) , conn);
+						//userDAO.addUser(new User(2, "Alexa", "Ghiurau", "0745147369", "alexa@mail.com", "alexa", "alexa", 1) , conn);
+						//update the phone number
+						//userDAO.updateUser(1, "0744631784", conn);
 						
+						//delete from database
+						//userDAO.deleteUser(1, conn);
+						
+						//show all the records
+						userDAO.showTheUsers(conn);
+						
+						conn.close();
 						
 					} catch (ClassNotFoundException e1) {
-						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					} catch (SQLException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					
 					
-		System.out.println("Goodbye!");
+		System.out.println("\n \nGoodbye!");
 	}
 }
--- src/UserManagement/UserDAO.java
+++ src/UserManagement/UserDAO.java
@@ -1,25 +1,89 @@
 package UserManagement;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UserDAO {
-	
-	public void addUser(User user, Connection conn){
-		Statement stmt = null;
-		
+
+	public void addUser(User user, Connection conn) throws SQLException {
+		Statement stmt = conn.createStatement();
+
 		String sql = "INSERT INTO user (idUser, firstName, lastName, phoneNumber, email, username, password, status) "
-				+ "VALUES ( " + user.getIdUser()+ ", '" + user.getFirstName() + "', '" + user.getLastName() 
-				+ "', '" + user.getPhoneNumber() + "', '" + user.getEmail() + "', '" + user.getUsername() + "', '"
+				+ "VALUES ( " + user.getIdUser() + ", '" + user.getFirstName() + "', '" + user.getLastName() + "', '"
+				+ user.getPhoneNumber() + "', '" + user.getEmail() + "', '" + user.getUsername() + "', '"
 				+ user.getPassword() + "', " + user.getStatus() + ");";
-				
+
 		try {
 			stmt.executeUpdate(sql);
+			stmt.close();
+
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
+	public void updateUser(int idUser, String newPhone, Connection conn) throws SQLException {
+		Statement stmt = conn.createStatement();
+
+		String sql = "UPDATE user " + "SET phoneNumber = '" + newPhone + "'  WHERE idUser= " + idUser + ";";
+		stmt.executeUpdate(sql);
+
+		try {
+			stmt.executeUpdate(sql);
+			stmt.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void deleteUser(int idUser, Connection conn) throws SQLException {
+		Statement stmt = conn.createStatement();
+
+		String sql = "DELETE FROM user WHERE idUser= " + idUser + ";";
+		stmt.executeUpdate(sql);
+
+		try {
+			stmt.executeUpdate(sql);
+			stmt.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showTheUsers(Connection conn) throws SQLException {
+		Statement stmt = conn.createStatement();
+
+		String sql = "SELECT idUser, firstName, lastName, phoneNumber, email, username, password, status FROM user";
+		ResultSet rs = stmt.executeQuery(sql);
+
+		while (rs.next()) {
+
+			// Retrieve by column name
+			int id = rs.getInt("idUser");
+			String firstName = rs.getString("firstName");
+			String lastName = rs.getString("lastName");
+			String phoneNumber = rs.getString("phoneNumber");
+			String email = rs.getString("email");
+			String username = rs.getString("username");
+			String password = rs.getString("password");
+			int status = rs.getInt("status");
+
+			// Display values
+			System.out.println();
+			System.out.print("ID: " + id);
+			System.out.print(", first name: " + firstName);
+			System.out.print(", last name: " + lastName);
+			System.out.print(", phone number: " + phoneNumber);
+			System.out.print(", email: " + email);
+			System.out.print(", username: " + username);
+			System.out.print(", password: " + password);
+			System.out.print(", status: " + status);
+
+		}
+		rs.close();
+	}
 }
