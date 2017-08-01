518297690b1bb91d8059b7584d2985a2bb2a1e3b
Damira Petrean <petrean.damira@gmail.com>
2017-08-01 10:31:37 +0300

    5 login fails beginning

---
+++ jbugs-client/src/main/java/edu/msg/ro/beans/CaptchaBean.java
@@ -0,0 +1,16 @@
+package edu.msg.ro.beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "captcha")
+@RequestScoped
+public class CaptchaBean {
+
+	public void submit() {
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Correct", "Correct");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+}
